0d88e5a40fec4a4b6e4e436a8832d117e6d9282a
Aidan Kang <akang@lcs.on.ca>
2023-04-06 10:23:29 -0400

    Initial Commit

---
+++ MoodMapper/MoodMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapperApp.swift
+//  MoodMapper
+//
+//  Created by Aidan Kang on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
