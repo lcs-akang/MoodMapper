281f28277f649d1c2edade7982a7f93f78ce5826
Aidan Kang <akang@lcs.on.ca>
2023-04-06 10:50:07 -0400

    Created MoodItem model

--- MoodMapper/Model/MoodItem.swift
+++ MoodMapper/Model/MoodItem.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct MoodItem: Identifiable {
+    var id: Int
+    var description: String
+    var emoji: String
+}
+
+var existingMoodItems = [
+
+    MoodItem(id: 1, description: "Fantastic", emoji: "👍")
+    
+    ,
+    
+    MoodItem(id: 2, description: "Great", emoji: "✅")
+
+    ,
+    
+    MoodItem(id: 3, description: "Sad", emoji: "😕")
+    
+    ,
+    
+]
