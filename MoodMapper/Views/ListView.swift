9623433753351c1919ae377bbd6a001274ff0eb0
Aidan Kang <akang@lcs.on.ca>
2023-04-06 10:28:07 -0400

    Organized project

---
+++ MoodMapper/Views/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  MoodMapper
+//
+//  Created by Aidan Kang on 2023-04-06.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
